371369d7efbb128d6f63cbc7411f46a978072317
Huy Do <huydhn@gmail.com>
2023-07-18 16:34:21 -0700

    Add the initial version of PR tokenizer using https://huggingface.co/bigcode/starcoderplus

---
+++ pr_embedding.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import json
+from transformers import AutoTokenizer
+from torch.utils.data import Dataset
+from typing import Any
+
+
+class PyTorchPRDataset(Dataset):
+    def __init__(self, pull_requests_file: str):
+        self.checkpoint = "bigcode/starcoderplus"
+        self.device = device
+        self.tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
+
+        with open(pull_requests_file) as f:
+            self.pull_requests = json.load(f)
+
+    def __len__(self):
+        return len(self.pull_requests)
+
+    def __getitem__(self, idx):
+        # TODO: Decide on what the input here would look like
+        patch = self.pull_requests[idx]["patch"]
+        return self.tokenizer.encode(patch)
+
+
+def parse_args() -> Any:
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser("GitHub PR embedding")
+    parser.add_argument("--input", type=str, help="the input JSON file")
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    data = PyTorchPRDataset(args.input)
+    print(data[0])
+
+
+if __name__ == "__main__":
+    main()
