2c7ee32eeb3cf20268648f30260fe97b57e50fb1
Zain Rizvi <ZainR@meta.com>
2023-07-19 15:29:20 -0500

    more caches

--- enumerate_test_files.py
+++ enumerate_test_files.py
@@ -10,3 +10,14 @@ def enumerate_test_files(repo_root_dir: Path):
             if file.endswith('.py') and file.startswith('test_'):
                 file_path = os.path.join(root, file)
                 yield file_path
+
+def tokenize_test_files(repo_root_dir: Path):
+    """
+    Returns a dictionary mapping test file names to their contents
+    """
+    test_files = enumerate_test_files(repo_root_dir)
+    test_file_to_content_mapping = {}
+    for test_file in test_files:
+        with open(test_file) as f:
+            test_file_to_content_mapping[test_file] = f.read()
+    return test_file_to_content_mapping
