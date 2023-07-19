5dd7ae4645613a28aeb781f9ef8515eecf019eac
Huy Do <huydhn@gmail.com>
2023-07-19 09:27:37 -0700

    Return tokenized PT tensor (same as tokenizing test files)

--- pr_tokenization.py
+++ pr_tokenization.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 import json
-from transformers import AutoTokenizer
-from torch.utils.data import Dataset
 from typing import Any
+
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
 
 
 class PyTorchPRDataset(Dataset):
     def __init__(self, pull_requests_file: str):
         self.checkpoint = "bigcode/starcoderplus"
         self.tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
         with open(pull_requests_file) as f:
             self.pull_requests = json.load(f)
@@ -18,10 +21,10 @@ class PyTorchPRDataset(Dataset):
         return len(self.pull_requests)
 
     def __getitem__(self, idx):
-        # TODO: Decide on what the input here would look like. This uses the following dummy JSON
-        # pytorch_prs_with_patch_100.json
+        # TODO: Decide on what the input here would look like. This uses the
+        # following dummy JSON pytorch_prs_with_patch_100.json
         patch = self.pull_requests[idx]["patch"]
-        return self.tokenizer.encode(patch)
+        return self.tokenizer.encode(patch, return_tensors="pt", padding=True)
 
 
 def parse_args() -> Any:
