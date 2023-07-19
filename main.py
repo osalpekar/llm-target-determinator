52be7e605308e1574be609c838761357bbaf5769
Omkar Salpekar <osalpekar@fb.com>
2023-07-18 17:37:32 -0700

    add support for embedding the testfiles

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+from argparse import ArgumentParser
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -15,7 +18,10 @@ all_data = [[text1, text2], [text3, text4]]
 class Indexer:
     def __init__(self):
         model_type = "bigcode/starcoderplus"
-        self.model = AutoModelForCausalLM.from_pretrained(model_type, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_type,
+            trust_remote_code=True
+        )
         self.tokenizer = AutoTokenizer.from_pretrained(model_type)
         self.test_index = None
 
@@ -24,7 +30,11 @@ class Indexer:
         self.index_to_testfile = {}
 
     def encode_batch(self, batched_input, debug=False):
-        tokenized_input = self.tokenizer(batched_input, return_tensors='pt', padding=True)
+        # tokenized_input = self.tokenizer(
+        #     batched_input,
+        #     return_tensors='pt',
+        #     padding=True
+        # )
 
         if debug:
             print(tokenized_input)
@@ -33,12 +43,17 @@ class Indexer:
         out = self.model(**tokenized_input)
         return out[1]
     
-    def index(self, all_data):
+    def index(self, tokens_file):
         embedding_list = []
 
-        for idx, batch in enumerate(all_data):
-            embeddings = self.encode_batch(batch, debug=False)
-            embedding_list.append(embeddings)
+        with open(tokens_file) as f:
+            data = json.load(f)
+
+        for idx, filename in enumerate(data):
+            embeddings = self.encode_batch(data[filename], debug=False)
+            embeddings_summed = torch.sum(embeddings, dim=0).reshape(1, 768)
+            embedding_list.append(embeddings_summed)
+            self.index_to_testfile[idx] = filename
 
         self.test_index = torch.cat(embedding_list, dim=0)
 
@@ -46,7 +61,7 @@ class Indexer:
 class TwoTower:
     def __init__(self, all_data):
         self.indexer = Indexer()
-        self.indexer.index(all_data)
+        self.indexer.index(tokens_file)
 
     def predict(self, diff):
         diff_embedding = self.indexer.encode_batch(batched_input)
@@ -58,15 +73,29 @@ class TwoTower:
         return similarity_matrix
         
 
+def parse_args() -> Any:
+    parser = ArgumentParser("Model Trainer")
+    parser.add_argument("--code-tokens", type=str, help="JSON file w PyTorch code")
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    self.indexer = Indexer()
+    self.indexer.index(args.code_tokens)
+
+
+if __name__ == "__main__":
+    main()
 
 ## Tests Code
-indexer = Indexer()
-output = indexer.encode_batch([text1, text2])
+# indexer = Indexer()
+# output = indexer.encode_batch([text1, text2])
 
-print(output.shape) # (batch_size x 768)
+# print(output.shape) # (batch_size x 768)
 # Below line will not work anymore since encode_batch now just returns
 # last_layer_states
 # print(output[2][-1].shape) # (batch_size x seq_len x 768)
 
-indexer.index(all_data)
-print(indexer.test_index.shape)
+# indexer.index(all_data)
+# print(indexer.test_index.shape)
