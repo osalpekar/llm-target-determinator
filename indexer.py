3f115cb4d6b1028587088f425ce7f790e701167f
Omkar Salpekar <osalpekar@fb.com>
2023-08-29 18:35:23 -0700

    Save Index to File

--- indexer.py
+++ indexer.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import datetime
 
 import torch
 
@@ -18,10 +19,10 @@ class Indexer:
 
         # Init Rank/Device
         try:
-            local_rank = os.environ["LOCAL_RANK"]
+            self.local_rank = os.environ["LOCAL_RANK"]
         except KeyError:
             # LOCAL_RANK may not be set if torchrun/torchx is not being used
-            local_rank = 0
+            self.local_rank = 0
 
         self.device = (
             torch.device(f"cuda:{local_rank}")
@@ -51,8 +52,17 @@ class Indexer:
             if idx == 2:
                 break
 
+        embeddings = torch.cat(embeddings)
         print(embeddings)
         print(function_list)
+        self.save_index(embeddings, function_list)
+
+    def save_index(self, embeddings, function_list):
+        rand = hash(datetime.now())
+        torch.save(embeddings, f"unittest_index_{rand}.pt")
+
+        with open(f"unittest_index_mapping_{rand}.json") as f:
+            json.dump({"mapping": function_list}, f)
 
 
 if __name__ == "__main__":
