d95bb57cf1c927fcc9d70ab65e70554a9f45babb
Omkar Salpekar <osalpekar@fb.com>
2023-09-06 16:04:29 -0700

    Redesign ast parsing to exclude nested and boilerplate functions to reduce batch size

--- preproc.py
+++ preproc.py
@@ -1,7 +1,7 @@
 import ast
 from collections import OrderedDict
 
-def get_functions(filename):
+def get_functions_old(filename):
     with open(filename) as f:
         # TODO: filelist can just have relative paths and we can call
         # expanduser here just before opening the file
@@ -34,3 +34,40 @@ def get_functions(filename):
 
     return functions
 
+
+class PythonVisitor(ast.NodeVisitor):
+    def __init__(self, filename, source):
+        super().__init__()
+        self.filename = filename
+        self.source_code = source
+        self.functions = OrderedDict()
+
+    def visit_FunctionDef(self, node):
+        signature = self.filename + ":" + node.name
+        body = ast.get_source_segment(self.source_code, node)
+        self.functions[signature] = body
+
+    def visit_ClassDef(self, node):
+        for sub_node in node.body:
+            if isinstance(sub_node, ast.FunctionDef):
+                # TODO: add a blocklist to not index functions like __init__,
+                # setUp, and tearDown
+                signature = (
+                    self.filename + ":" + node.name + "." + sub_node.name
+                )
+                body = ast.get_source_segment(self.source_code, sub_node)
+                self.functions[signature] = body
+
+def get_functions(filename):
+    with open(filename) as f:
+        # TODO: filelist can just have relative paths and we can call
+        # expanduser here just before opening the file
+        file_content = f.read()
+
+    functions = OrderedDict()
+
+    visitor = PythonVisitor(filename, file_content)
+    tree = ast.parse(file_content)
+    visitor.visit(tree)
+
+    return visitor.functions
